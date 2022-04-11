8a2fd3a492cb6b04a4ba8799df783bc1707b837f
lishengpeng <qc_22d4bcf3b66d4d26809596a008b569e0@git.code.tencent.com>
2022-04-11 10:40:08 +0800

    first commit

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,16 @@
+<template>
+ <div id="wrapper">12213123aaa</div>
+</template>
+
+<script>
+export default {
+ name: 'landing-page',
+ methods: {
+  open(link) {
+   this.$electron.shell.openExternal(link)
+  },
+ },
+}
+</script>
+
+<style></style>
